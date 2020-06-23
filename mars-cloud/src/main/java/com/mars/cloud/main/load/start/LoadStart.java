64ad0f89d40163150ebbb033a453d33a56d5bccf
yuye <631941527@qq.com>
2020-06-23 21:23:43 +0800

    补充注释

--- mars-cloud/src/main/java/com/mars/cloud/main/load/start/LoadStart.java
+++ mars-cloud/src/main/java/com/mars/cloud/main/load/start/LoadStart.java
@@ -4,9 +4,15 @@ import com.mars.cloud.main.load.register.Registered;
 import com.mars.common.annotation.bean.MarsBean;
 import com.mars.common.annotation.bean.MarsWrite;
 
+/**
+ * 加载
+ */
 @MarsBean("loadStart")
 public class LoadStart {
 
+    /**
+     * 注册业务逻辑所在的类
+     */
     @MarsWrite("registered")
     private Registered registered;
 
