b450a1c03a86ff9e3440c962eff910076a2cbad2
yuye <631941527@qq.com>
2020-10-03 22:06:50 +0800

    支持转发请求头

--- mars-gateway-starter/src/main/java/com/mars/gateway/request/RequestServer.java
+++ mars-gateway-starter/src/main/java/com/mars/gateway/request/RequestServer.java
@@ -2,10 +2,14 @@ package com.mars.gateway.request;
 
 import com.mars.cloud.request.rest.request.MarsRestTemplate;
 import com.mars.cloud.request.util.model.HttpResultModel;
+import com.mars.cloud.request.util.model.MarsHeader;
 import com.mars.gateway.api.model.RequestInfoModel;
 import com.mars.gateway.request.param.ParamConversionToModel;
 import com.mars.gateway.request.util.RequestUtil;
 import com.mars.server.server.request.HttpMarsRequest;
+import com.sun.net.httpserver.Headers;
+
+import java.util.List;
 
 /**
  * 转发给对应的微服务
@@ -22,10 +26,33 @@ public class RequestServer {
     public static HttpResultModel doRequest(RequestInfoModel requestInfoModel, HttpMarsRequest request) throws Exception {
         Object params = ParamConversionToModel.paramConversionToMap(request);
 
+        MarsHeader header = getHeader(request);
+
         return MarsRestTemplate.request(requestInfoModel.getServerName(),
                 requestInfoModel.getMethodName(),
                 new Object[]{params},
                 HttpResultModel.class,
-                RequestUtil.getContentType(request));
+                RequestUtil.getContentType(request),header);
+    }
+
+    /**
+     * 获取请求头
+     * @param request
+     * @return
+     */
+    private static MarsHeader getHeader(HttpMarsRequest request){
+        Headers headers = request.getHttpExchange().getRequestHeaders();
+        if(headers == null || headers.size() < 1){
+            return null;
+        }
+        MarsHeader marsHeader = new MarsHeader();
+        for(String name : headers.keySet()){
+            List<String> values = headers.get(name);
+            if(values == null || values.size() < 1){
+                continue;
+            }
+            marsHeader.put(name, values);
+        }
+        return marsHeader;
     }
 }
